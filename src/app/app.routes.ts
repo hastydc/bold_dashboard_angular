483e8f140733ad36601bb941767c5dc06b832093
Efrain Mancilla <hastydc@gmail.com>
2024-06-12 18:25:03 -0500

    feat: 🎸 init project
    init project with sass for functions, mixins, colors, theme and
    bashboard page route and clean app component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
